978b95889de07383c81f5d3bb83ffe1fafb1b279
MumuCodeuse <m.ralu@orange.fr>
2026-04-01 15:04:50 +0200

    Refactor mission form using FormData and start new extended probe form and probe types page

--- public/js/forms/missionForm.js
+++ public/js/forms/missionForm.js
@@ -4,58 +4,64 @@
 const form = document.getElementById("formMission");
 
 // Construire l'envoi du formulaire
-form.addEventListener("submit", async function(event) {
-  
-  const missionName = document.getElementById("name").value;
-// Console.log  
-  console.log("missionName (front) :", missionName);
-  if (missionName === "") {
-    //Empêcher soumission du formulaire par défaut si la mission n'est pas nomée
-    event.preventDefault(); // bloquer
+form.addEventListener("submit", async function (event) {
+  event.preventDefault();
+  const formDataForm = new FormData(form);
+
+  // Récupérer les clés/valeurs de chaque input
+  const missionName = formDataForm.get("missionName");
+  const missionStartYear = formDataForm.get("missionStartYear");
+  const missionEndYear = formDataForm.get("missionEndYear");
+  const missionObjective = formDataForm.get("missionObjective");
+  const missionImageUrl = formDataForm.get("missionImageUrl");
+
+  // console.log("missionName (front) :", missionName);
+  if (missionName.length === 0) {
+    //Empêcher soumission du formulaire par défaut si la mission n'est pas nommée
+    event.preventDefault();
     alert("Le nom de la mission doit être indiqué"); // alerter
     return; // stopper
   }
-  // Empêcher la soumission par défaut
-  event.preventDefault();
 
-  //Récupérer les données du formulaire et construire un objet js
-  const formData = new FormData(event.target);
-  const objectData = {};
-  formData.forEach((value, key) => {
-    objectData[key] = value;
-// Console.log
-    console.log(event.target);
-  });
+  // Construction de l'objet envoyé au backend
+  const missionData = {
+    missionName,
+    missionStartYear,
+    missionEndYear,
+    missionObjective,
+    missionImageUrl,
+  };
 
-  console.log("Données envoyées au back :", {
-  missionName,
-  missionStartYear,
-  missionEndYear,
-  missionObjective,
-  missionImageUrl
-  });
+  // console.log("Données envoyées au back :", {
+  //   missionName,
+  //   missionStartYear,
+  //   missionEndYear,
+  //   missionObjective,
+  //   missionImageUrl,
+  // });
 
   //Envoyé l'objet au backend via fetch
-
   try {
     const res = await fetch("http://localhost:3000/api/missions", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(objectData),
+      body: JSON.stringify(missionData),
     });
     // Lire la réponse du backend
-    const dataMission = await res.json();
-// Console.log
-    console.log("Réponse du back :", dataMission);
+
+    const resFetchDataMission = await res.json();
+    // Console.log
+    console.log("Réponse du back :", resFetchDataMission);
     if (!res.ok) {
       return alert(
-        "La mission n'a pas été créée : " + (dataMission.errorMessage || ""),
+        "La mission n'a pas été créée : " +
+          (resFetchDataMission.errorMessage || ""),
       );
     }
     alert("Mission créée avec succès");
-    document.getElementById("missionForm").reset();
+    form.reset();
   } catch (error) {
-// Console.log
+    // Console.log
     console.error("Erreur réseau :", error);
     alert("Erreur : impossible de contacter le serveur.");
   }
